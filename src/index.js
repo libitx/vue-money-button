ffde89e2ca3d933be335a6264593585741467094
libitx <libitx@protonmail.com>
2021-01-26 23:36:45 +0000

    Vue 3 compat. Ref #7

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import MoneyButton from './components/MoneyButton.vue'
 import scriptLoader from './script-loader'
 
 export default {
-  install(Vue) {
-    Vue.component('MoneyButton', MoneyButton)
-    Vue.prototype.$getMoneyButton = scriptLoader.load
+  install(app) {
+    app.component('MoneyButton', MoneyButton)
+    // Vue v3.x
+    if (app.version.substr(0, 1) >= 3) {
+      app.config.globalProperties.$getMoneyButton = scriptLoader.load
+    // Vue v2.x
+    } else {
+      app.prototype.$getMoneyButton = scriptLoader.load
+    }
   }
 }
 
